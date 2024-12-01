81539a372201ad956fea3872f16af4287fc4f670
Md Aman <aman.nit.cse@gmail.com>
2024-12-01 08:21:08 +0530

    update

--- Day02/calculator.py
+++ Day02/calculator.py
@@ -1,1 +1,10 @@
 
+a = 46
+b = 34
+
+print("the vale of",a, "+",b, "is: ", a+b)
+print("the vale of",a, "-",b, "is: ", a-b)
+print("the vale of",a, "*",b, "is: ", a*b)
+print("the vale of",a, "/",b, "is: ", a/b)
+print("the vale of",a, "//",b, "is: ", a//b)
+#print("the vale of",a, "**",b, "is: ", a**b)
