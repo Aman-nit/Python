7e8a74a7ebf584db08e9adc6ea57165a8f513f47
Md Aman <aman.nit.cse@gmail.com>
2024-12-09 19:17:04 +0530

    Added

---
+++ Day10/creatCustomError.py
@@ -0,0 +1,9 @@
+a = int(input("Enter any value between 5 and 9"))
+
+if(a<5  or a>9):
+  raise  ValueError("Value should be between 5 and 9")
+
+
+def customError ():
+  a = input("Enter any value between 5 and 9")
+  
